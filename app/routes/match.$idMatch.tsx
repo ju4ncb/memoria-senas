76e1729510f962dbf57f32054961ede31853ddfe
ju4ncb <ju4ncb@gmail.com>
2025-11-20 10:22:29 -0500

    feat: set match finished state on match page initialization

--- app/routes/match.$idMatch.tsx
+++ app/routes/match.$idMatch.tsx
@@ -441,6 +441,7 @@ export default function MatchPage() {
         return;
       getCurrentMatch();
       fetchSlots();
+      setFinished(match.state === "finished");
     };
 
     // Initial check
